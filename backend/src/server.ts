d5714f20f726531e3ae7a947b915be96cd8530d0
Vensan <v.drot@student.inha.uz>
2022-09-28 00:59:12 +0500

    front back

---
+++ backend/src/server.ts
@@ -0,0 +1,86 @@
+import express, { Request, Response } from "express";
+import session from 'express-session';
+import { PrismaClient } from "@prisma/client";
+import bodyParser from "body-parser";
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
+var jsonParser = bodyParser.json();
+const prisma = new PrismaClient();
+const app = express();
+
+
+
+// parse application/json
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(express.json());
+app.use(cors({
+  origin: ['http://localhost:3000'],
+  methods: ['POST', 'GET'],
+  credentials: true
+}))
+
+
+
+// sql requests to mysql db
+app.post("/createuser", async (req: Request, res: Response) => {
+  console.log(req.body);
+  const { name, pass, email } = req.body;
+  const user = await prisma.user.create({
+    data: {
+      name: name,
+      password: pass,
+      email: email,
+    },
+  });
+  res.json(user);
+});
+
+app.get("/get", async (req: Request, res: Response) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+
+app.post("/getuser", async (req: Request, res: Response) => {
+  const { email, pass } = req.body;
+  const user = await prisma.user.findMany({
+    where: {
+      email: email,
+      password: pass,
+    },
+  });
+  res.json(user);
+});
+
+
+app.delete("/:id", async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const deleted_user = await prisma.user.delete({
+    where: {
+      id: Number(id),
+    },
+  });
+  res.json(deleted_user);
+});
+
+
+// Session part idk if it works or not yet
+
+app.use(
+  session({
+    //key: "UserId",
+    secret: "subscribe",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 3600 * 24,
+    }
+  })
+);
+
+
+
+app.listen(3001, () => console.log("listening on port 3001"));
---
+++ frontend/src/components/nav/Nav.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import "./nav.css";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  const [status, setStatus] = useState("close");
+  const [statusdiv, setStatusdiv] = useState("out");
+
+  function stche() {
+    setStatus(status === "open" ? "close" : "open");
+    setStatusdiv(statusdiv === "in" ? "out" : "in");
+  }
+
+  return (
+    <>
+      <nav>
+        <Link to="/" className="vensan">
+          VensanStore
+        </Link>
+        <ul className="div">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/login">login</Link>
+          </li>
+        </ul>
+
+        <div
+          className="BurgerMenu__container"
+          role="button"
+          onClick={() => stche()}
+        >
+          <i className={status}></i>
+          <i className={status}></i>
+          <i className={status}></i>
+        </div>
+
+        <ul className="div-mobile" className={statusdiv}>
+          <li>
+            <Link
+              to="/"
+              onClick={() => {
+                setStatusdiv(statusdiv === "in" ? "out" : "in");
+                setStatus(status === "open" ? "close" : "open");
+              }}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/register"
+              onClick={() => {
+                setStatusdiv(statusdiv === "in" ? "out" : "in");
+                setStatus(status === "open" ? "close" : "open");
+              }}
+            >
+              Register
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Nav;
